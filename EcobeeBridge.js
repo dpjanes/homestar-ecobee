b3089736208675f4841bdbdd8e95cf2e115e85f1
David Janes <davidjanes@davidjanes.com>
2016-01-24 13:30:50 -0500

    remove bunyan reference

--- EcobeeBridge.js
+++ EcobeeBridge.js
@@ -24,11 +24,10 @@
 
 var iotdb = require('iotdb');
 var _ = iotdb._;
-var bunyan = iotdb.bunyan;
 
 var ecobee = require('ecobee');
 
-var logger = bunyan.createLogger({
+var logger = iotdb.logger({
     name: 'homestar-ecobee',
     module: 'EcobeeBridge',
 });
